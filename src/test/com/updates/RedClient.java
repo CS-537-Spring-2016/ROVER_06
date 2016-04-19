7d7bf47dd25f40d8d32547faf28e7fe487078a37
shay <shay.nguyen@gmail.com>
2016-04-18 21:02:32 -0700

    Fix silly errors / commit for test.com.updates
    previous commit or two i wasn't paying attention and accidently deleted
    some stuff and merge some other stuff that have no business being merged

--- src/test/com/updates/RedClient.java
+++ src/test/com/updates/RedClient.java
@@ -6,7 +6,7 @@ package test.com.updates;
  * It will connects to the UpdateServer, read the server message, and prints it out onto the console. 
  */
 public class RedClient {
-
+    
     public static void main(String[] args) {
         Client client = new Client("localhost", 9000, "RED");
         client.run();
