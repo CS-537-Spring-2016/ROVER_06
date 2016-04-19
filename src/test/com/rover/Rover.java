0d75b5724fc323d0ad3df0f7ba84992009bc04f0
shay <shay.nguyen@gmail.com>
2016-04-18 21:05:50 -0700

    rearrange all the threads
    instead of having all the threads in separate classes, i moved them to
    the same class, Rover, because they all share similar logic and will
    probably not be reuseable anyways

--- src/test/com/rover/Rover.java
+++ src/test/com/rover/Rover.java
@@ -1,10 +1,13 @@
 package test.com.rover;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /*
  * This is a striped down test version of our Rover.
@@ -114,6 +117,47 @@ public class Rover {
                 }
             }
         }
+    }
+    
+    public class RoverCommunicationClientsThread implements Runnable {
 
+        private Socket socket;
+        private String host;
+        private int port;
+
+        public RoverCommunicationClientsThread(String host, int port) {
+            this.host = host;
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                while (socket == null) {
+                    try {
+                        socket = new Socket(host, port);
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        System.out.println("waiting for host ...");
+                    }
+                }
+                System.out.println("Rover successfully connected to a host!");
+                System.out.println("Waiting for a message from other Rovers ...");
+
+                // initialize i/o
+                BufferedReader inFromServer = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+
+                while (true) {
+                    // read and display message from other Rovers
+                    String msg = inFromServer.readLine();
+                    System.out.println(msg);
+                }
+            } catch (IOException ex2) {
+                ex2.printStackTrace();
+            }
+        }
     }
 }
