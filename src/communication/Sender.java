60535d2e9068da5c0cc7040bbbc1724619e77b1d
shay <shay.nguyen@gmail.com>
2016-05-22 21:12:47 -0700

    Update interface. No longer requires output stream. Use Group Instead
    Group class contain IP and PORT number. Will use it to create a socket,
    output stream, communicate, then close the stream immediately .

--- src/communication/Sender.java
+++ src/communication/Sender.java
@@ -1,6 +1,5 @@
 package communication;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -11,12 +10,11 @@ import common.Coord;
  * @author Shay */
 public interface Sender {
 
-    /** @param outputStreams
-     *            A list of all the ROVER's output stream. Will used this to
-     *            write a message to each individual ROVER's socket
+    /** @param groups
+     *            A list of all the groups (ROVER INFO). Will used this to write
+     *            a message to each individual ROVER's socket
      * @param coord
      *            The coordinate that you want to share to other ROVERS
      * @throws IOException */
-    void shareScience(List<DataOutputStream> outputStreams, Coord coord) throws IOException;
-
+    void shareScience(List<Group> groupList, Coord coord);
 }
