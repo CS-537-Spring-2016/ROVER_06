b9a1b90898eeca268b1265362c30054f4e662be1
shay <shay.nguyen@gmail.com>
2016-05-22 21:09:00 -0700

    Update Receiver. Open/close stream after finish. Add comments/refactor

--- src/communication/RoverReceiver.java
+++ src/communication/RoverReceiver.java
@@ -13,41 +13,33 @@ import common.Coord;
 import enums.Science;
 import enums.Terrain;
 
-/** When any ROVER discovered science, it will write a message to your all
- * ROVERS. That message will be "sent" here.
+/** Communication Module. Receive science and process science coordinates
  * 
  * @author Shay */
 public class RoverReceiver implements Receiver {
 
     private ServerSocket listenSocket;
-    private List<Coord> sharedScienceCoords;
-    private List<Terrain> ignoredTerrains;
-    private int roversConnectedToMe;
 
-    public RoverReceiver() throws IOException {
-        sharedScienceCoords = new ArrayList<Coord>();
-        roversConnectedToMe = 0;
-    }
+    /** Coordinates of all science share to this receiver from other ROVERS */
+    private List<Coord> sharedScienceCoords = new ArrayList<Coord>();
 
-    @Override
-    public int getRoversConnectedToMe() {
-        return roversConnectedToMe;
-    }
+    /** Terrains to ignore. Used to filter out Science coordinate */
+    private List<Terrain> ignoredTerrains = new ArrayList<Terrain>();
 
     @Override
     public void startServer(ServerSocket serverSocket) throws IOException {
         listenSocket = serverSocket;
-        
-        // create a thread that waits for client to connect to
+
+        /* Create a thread that waits for client to connects */
         new Thread(() -> {
             while (true) {
                 try {
-                    // wait for a connection
+                    /* wait for a connection */
                     Socket connectionSocket = listenSocket.accept();
 
-                    // once there is a connection, serve them on a separate thread
+                    /* once there is a connection, serve them on a separate
+                     thread */
                     new Thread(new RoverHandler(connectionSocket)).start();
-                    roversConnectedToMe++;
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -57,16 +49,19 @@ public class RoverReceiver implements Receiver {
     }
 
     @Override
+    /**@return List of all the coordinate shared from other ROVERS */
     public List<Coord> getSharedCoords() {
         return sharedScienceCoords;
     }
 
     @Override
+    /** Set the Terrains to ignore by this receiver */
     public void ignoreTerrains(Terrain... terrains) {
         ignoredTerrains = Arrays.asList(terrains);
     }
 
     @Override
+    /**@return A list of Terrain this receiver is ignoring*/
     public List<Terrain> getIgnoredTerrains() {
         return ignoredTerrains;
     }
@@ -76,7 +71,7 @@ public class RoverReceiver implements Receiver {
      * @author Shay */
     class RoverHandler implements Runnable {
 
-        Socket roverSocket;
+        private Socket roverSocket;
 
         public RoverHandler(Socket socket) {
             this.roverSocket = socket;
@@ -85,32 +80,42 @@ public class RoverReceiver implements Receiver {
         @Override
         public void run() {
 
+            BufferedReader input = null;
             try {
-                BufferedReader input = new BufferedReader(
+                input = new BufferedReader(
                         new InputStreamReader(roverSocket.getInputStream()));
-
-                while (true) {
-
-                    String[] line = input.readLine().split(" ");
-                    // protocol: ROCK CRYSTAL 25 30
-
+                String[] line = input.readLine().split(" ");
+                
+                /* protocol: ROCK CRYSTAL 25 30 */
+                /* parse and process the input */
+                if (line.length == 4) {
+                    Terrain terrain = Terrain.valueOf(line[0]);
+                    Science science = Science.valueOf(line[1]);
+                    int xpos = Integer.valueOf(line[2]);
+                    int ypos = Integer.valueOf(line[3]);
+                    Coord coord = new Coord(terrain, science, xpos, ypos);
+                    
+                    /* Add Coordinate if possible*/
+                    updateShareScience(coord);
+                }
+               
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(input != null) {
                     try {
-                        if (line.length == 4) {
-                            Terrain terrain = Terrain.valueOf(line[0]);
-                            Science science = Science.valueOf(line[1]);
-                            int xpos = Integer.valueOf(line[2]);
-                            int ypos = Integer.valueOf(line[3]);
-
-                            if (!ignoredTerrains.contains(terrain))
-                                sharedScienceCoords.add(new Coord(terrain, science, xpos, ypos));
-                        }
-
-                    } catch (Exception e) {
+                        input.close();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+        }
+        
+        /** If the Science Coordinate is not on an "ignored terrains" and is unique, add it to "shared science list"*/
+        private void updateShareScience(Coord coord) {
+            if (!ignoredTerrains.contains(coord.terrain) && !sharedScienceCoords.contains(coord)) {
+                sharedScienceCoords.add(coord);
             }
         }
     }
