c2e304f3c33409f8079ac6793e2e0f7a2a6a7bc9
shay <shay.nguyen@gmail.com>
2016-05-22 18:13:27 -0700

    Set up Command Center

--- src/command_center/CommandCenter.java
+++ src/command_center/CommandCenter.java
@@ -2,11 +2,14 @@ package command_center;
 
 import java.io.IOException;
 
+import communication.Group;
+
 public class CommandCenter {
     
     public final static int PORT = 53799;
     public final static String NAME = "Blue Command Center";
     public CommandServer commandServer;
+    public final static int TIME_TILL_NEXT_BROADCAST = 5000;
     
     
     public CommandCenter(String name, int port) {
@@ -16,6 +19,8 @@ public class CommandCenter {
     public static void main(String[] args) throws IOException {
         
         CommandServer commandCenter = new CommandServer(NAME, PORT);
+        commandCenter.addToBroadcastGroup(Group.G3);
+        commandCenter.setTimeWaitTillNextBroadcast(TIME_TILL_NEXT_BROADCAST);
         commandCenter.startServer();
     }
 }
